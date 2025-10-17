00537b39b476d32128fcd621f36c605951c25452
Rafalerio <rafael.avalerio001@gmail.com>
2025-10-17 18:25:34 -0300

    criação do repository

---
+++ AutoMarketAPI/src/main/java/com/MecanicaCeleste/AutoMarketAPI/AutoMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.MecanicaCeleste.AutoMarketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMarketApiApplication.class, args);
+	}
+
+}
---
+++ AutoMarketAPI/src/test/java/com/MecanicaCeleste/AutoMarketAPI/AutoMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MecanicaCeleste.AutoMarketAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
